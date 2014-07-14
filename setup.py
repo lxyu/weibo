f057ab67c81e828a3f9225f6900aab68c961e4a8
lxyu <lixinfish@gmail.com>
2014-07-14 17:13:59 +0800

    update setup.py and version bump

--- setup.py
+++ setup.py
@@ -5,16 +5,23 @@ from distutils.core import setup
 
 setup(
     name='weibo',
-    version='0.2.0',
+    version='0.2.1',
     description='Python sina weibo sdk',
     author='Lx Yu',
-    author_email='github@lxyu.net',
+    author_email='i@lxyu.net',
     py_modules=['weibo', ],
-    package_data={'': ['LICENSE'], },
     url='http://lxyu.github.io/weibo/',
     license="BSD",
     long_description=open('README.rst').read(),
     install_requires=[
-        "requests",
+        "requests>=2.3.0",
     ],
-)
+    classifiers=[
+        "Topic :: Software Development",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ])
